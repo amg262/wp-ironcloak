48c58c94fe0c9804f24694ac1db3b1592e8948ed
Andrew Gunn <andrewmgunn26@gmail.com>
2019-05-21 02:50:26 -0500

    sdfsd

--- gulpfile.js
+++ gulpfile.js
@@ -20,82 +20,92 @@ const unzip = require('gulp-unzip');
 const minimatch = require('minimatch');
 const mkdirp = require('mkdirp');
 
-var paths = {
-	acf: 'includes/acf/',
-	app: 'app/',
-	containers: 'app/containers/',
-	utils: 'app/utils/',
-	test: 'test/',
-	api: 'includes/api/',
-	native: 'native/',
-	electron: 'native/electron-quick-start/',
-	test: 'includes/api/',
-	vendor: 'vendor/',
-	node: 'node_modules/',
-	webpack: 'webpack.config.js/',
-	yml: '_config.yml/',
-	yarn: 'yarn.*/',
-	composer: 'composer.json/',
-	gulp: 'gulpfile.js/',
-	npm: 'package.json/',
-	yarn: 'yarn.*/',
-	assets: 'assets/',
-	classes: 'classes/',
-	data: 'assets/data/',
-	dist: 'dist/',
-	logs: 'logs/',
-	includes: 'includes/',
-	images: 'assets/images/*.png',
-	js: 'assets/js/*.js',
-	css: 'assets/css/*',
-	min_js: 'dist/js/*',
-	min_css: 'dist/css/*',
-	min_img: 'dist/images/',
-	log: 'assets/log/',
-	backups: 'assets/backups/',
-	dist_data: 'dist/data/',
-	archive: 'dist/data/archive.zip',
-	exports: 'assets/export/',
-	endpoint: 'includes/Endpoint/',
-	img: 'assets/images/*',
-	archive: 'assets/archive/',
-	dist_js: 'dist/',
-	dist_css: 'dist/',
-	dist_img: 'dist/images/',
-	host: 'http://localhost/wp/wp-admin',
-	php: '*.php',
-	wpb: 'wp-bom.php',
+//var paths = {
+//	acf: 'includes/acf/',
+//	app: 'app/',
+//	containers: 'app/containers/',
+//	utils: 'app/utils/',
+//	test: 'test/',
+//	api: 'includes/api/',
+//	native: 'native/',
+//	electron: 'native/electron-quick-start/',
+//	test: 'includes/api/',
+//	vendor: 'vendor/',
+//	node: 'node_modules/',
+//	webpack: 'webpack.config.js/',
+//	yml: '_config.yml/',
+//	yarn: 'yarn.*/',
+//	composer: 'composer.json/',
+//	gulp: 'gulpfile.js/',
+//	npm: 'package.json/',
+//	yarn: 'yarn.*/',
+//	assets: 'assets/',
+//	classes: 'classes/',
+//	data: 'assets/data/',
+//	dist: 'dist/',
+//	logs: 'logs/',
+//	includes: 'includes/',
+//	images: 'assets/images/*.png',
+//	js: 'assets/js/*.js',
+//	css: 'assets/css/*',
+//	min_js: 'dist/js/*',
+//	min_css: 'dist/css/*',
+//	min_img: 'dist/images/',
+//	log: 'assets/log/',
+//	backups: 'assets/backups/',
+//	dist_data: 'dist/data/',
+//	archive: 'dist/data/archive.zip',
+//	exports: 'assets/export/',
+//	endpoint: 'includes/Endpoint/',
+//	img: 'assets/images/*',
+//	archive: 'assets/archive/',
+//	dist_js: 'dist/',
+//	dist_css: 'dist/',
+//	dist_img: 'dist/images/',
+//	host: 'http://localhost/wp/wp-admin',
+//	php: '*.php',
+//	wpb: 'wp-bom.php',
+//};
+
+var dirs = {
+
+	all: '*',
+	js: '*.js',
+	ic: 'wp-ironcloak.php',
+	g: 'gulpfile.js',
+	p: '*.php',
+	n: '/*/',
 };
 
-// Not all tasks need to use streams
-// A gulpfile is just another node program and you can use any package available on npm
-gulp.task('purge', function () {
-	gulp.src('js/*').pipe(clean());
-	gulp.src(paths.dist + 'css/*').pipe(clean());
-});
-
-// Copy all static images
-gulp.task('imagemin', function () {
-	gulp.src(paths.images).pipe(imagemin()).pipe(gulp.dest(paths.min_img));
-});
-
-gulp.task('cssnano', function () {
-	gulp.src(paths.css).pipe(cssnano()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('scripts', function () {
-	return gulp.src(paths.data).pipe(concat('*')).pipe(gulp.dest('archive'));
-});
-
-/**
- * Minify compiled JavaScript.
- *
- * https://www.npmjs.com/package/gulp-uglify
- */
-gulp.task('uglify', function () {
-
-	gulp.src(paths.js).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist/js'));
-});
+//// Not all tasks need to use streams
+//// A gulpfile is just another node program and you can use any package available on npm
+//gulp.task('purge', function () {
+//	gulp.src('js/*').pipe(clean());
+//	gulp.src(paths.dist + 'css/*').pipe(clean());
+//});
+//
+//// Copy all static images
+//gulp.task('imagemin', function () {
+//	gulp.src(paths.images).pipe(imagemin()).pipe(gulp.dest(paths.min_img));
+//});
+//
+//gulp.task('cssnano', function () {
+//	gulp.src(paths.css).pipe(cssnano()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist/css'));
+//});
+//
+//gulp.task('scripts', function () {
+//	return gulp.src(paths.data).pipe(concat('*')).pipe(gulp.dest('archive'));
+//});
+//
+///**
+// * Minify compiled JavaScript.
+// *
+// * https://www.npmjs.com/package/gulp-uglify
+// */
+//gulp.task('uglify', function () {
+//
+//	gulp.src(paths.js).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist/js'));
+//});
 
 
 
@@ -111,15 +121,16 @@ gulp.task('serve', function () {
 // Rerun the task when a file changes
 gulp.task('watch', function () {
 	//gulp.watch(paths.scripts, ["scripts"]);
-	gulp.watch(paths.wpb).on('change', browserSync.reload);
-	gulp.watch(paths.includes + '*').on('change', browserSync.reload);
-	gulp.watch(paths.classes + '*').on('change', browserSync.reload);
-	gulp.watch(paths.assets + '*').on('change', browserSync.reload);
-	gulp.watch(paths.dist + '*').on('change', browserSync.reload);
+	gulp.watch(dirs.all).on('change', browserSync.reload);
+	gulp.watch(dirs.g).on('change', browserSync.reload);
+	gulp.watch(dirs.ic).on('change', browserSync.reload);
+	gulp.watch(dirs.js).on('change', browserSync.reload);
+	gulp.watch(dirs.p).on('change', browserSync.reload);
+	gulp.watch(dirs.n).on('change', browserSync.reload);
 });
 
-
-gulp.task('clean', ['purge', 'imagemin', 'cssnano', 'uglify']);
-gulp.task('run', ['purge', 'imagemin', 'cssnano', 'uglify', 'zip', 'scripts', 'serve', 'watch']);
+//
+//gulp.task('clean', ['purge', 'imagemin', 'cssnano', 'uglify']);
+//gulp.task('run', ['purge', 'imagemin', 'cssnano', 'uglify', 'zip', 'scripts', 'serve', 'watch']);
 gulp.task('live', ['serve', 'watch']);
 
